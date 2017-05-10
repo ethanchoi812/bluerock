1f0572140a22bacac86323c61254c2a25e2db36e
Ethan Choi <ethanchoi.812@gmail.com>
2017-05-10 17:58:16 +0800

    Update to styles for site responsiveness

--- script.js
+++ script.js
@@ -2,13 +2,11 @@
 
 	window.onscroll = function(){
 		changeNavBgScroll();
-		
-		//var abtDiv = document.getElementById("about");
-		//var svcDiv = document.getElementById("services");
+		activeNav();
+
 		var faicons = document.getElementsByClassName("fai");
 		var getStartedBtn = document.getElementById("get-started-btn");
-		var downloadBtn = document.getElementById("download-btn");
-
+		var downloadBtn = document.getElementById("download-btn");	
 
 		for (var i=0; i<faicons.length; i++){			
 			lazyLoad(faicons[i]);
@@ -18,10 +16,6 @@
 		lazyLoad(downloadBtn);
 	};
 	
-	window.onresize = function(){
-		changeNavBgResize();
-	}
-
 	function changeNavBgScroll(){
 
 		var mainNav = document.getElementById("main-nav");
@@ -29,17 +23,6 @@
 		if (document.body.scrollTop > 70){ 
 			mainNav.classList.add("white-nav");
 		} else if (document.body.scrollTop < 70) {
-			mainNav.classList.remove("white-nav");
-		}
-	};
-
-	function changeNavBgResize(){
-
-		var mainNav = document.getElementById("main-nav");
-
-		if (document.body.scrollTop > 70 || window.innerWidth < 765){ 
-			mainNav.classList.add("white-nav");
-		} else if (document.body.scrollTop < 70 || window.innerWidth > 765) {
 			mainNav.classList.remove("white-nav");
 		}
 	};
@@ -61,5 +44,12 @@
 			el.classList.add("zoomIn");
 		}
 	}
+
+	function activeNav(){
+
+		//todo
+		
+	}
+	
 		
 	
